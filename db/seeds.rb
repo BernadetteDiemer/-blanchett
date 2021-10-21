961103cff73d579b9269c188fb41284f09298fc5
Nathalie Faber <nathalie_faber@hotmail.com>
2021-10-21 20:01:04 +0200

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,64 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Booking.destroy_all
 
-User.create(first_name: 'Sarah', last_name: 'Smith', email: 'sarah_smith@gmail.com', password: 'hello123')
-User.create(first_name: 'Nathalie', last_name: 'Faber', email: 'nathaliefaber@hotmail.com', password: 'funkystuff123')
+sarah  = User.create! (
+  first_name: 'Sarah',
+  last_name: 'Smith',
+  email: 'sarah_smith@gmail.com',
+  password: 'hello123')
+
+nathalie = User.create! (
+  first_name: 'Nathalie',
+  last_name: 'Faber',
+  email: 'nathaliefaber@hotmail.com',
+  password: 'funkystuff123')
 
 puts '2 users created'
+
+booking_1 = booking.create!(
+  start_datetime: '2 May 2022',
+  end_datetime: '3 May 2022',
+  status: 'confirmed',
+  user: nathalie,
+)
+
+booking_2 = booking.create!(
+  start_datetime: '10 Nov 2022',
+  end_datetime: '11 Nov 2022',
+  status: 'pending',
+  user: sarah,
+)
+
+booking_3 = booking.create!(
+  start_datetime: '22 Dec 2022',
+  end_datetime: '22 Dec 2022',
+  status: 'cancelled',
+  user: sarah,
+)
+puts '3 bookings created'
+
+art_service_1 = art_service.create!(
+  title:
+  description:
+  price:
+  address:
+  category:
+)
+
+art_service_1 = art_service.create!(
+  title:
+  description:
+  price:
+  address:
+  category:
+)
+
+art_service_1 = art_service.create!(
+  title:
+  description:
+  price:
+  address:
+  category:
+)
