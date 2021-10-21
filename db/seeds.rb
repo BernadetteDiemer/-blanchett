c8a590594c1aed44891a6fd688273d8d503877fe
Nathalie Faber <nathalie_faber@hotmail.com>
2021-10-21 21:52:41 +0200

    added the seeds- needs work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,65 +5,75 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
+require 'date'
+ArtService.destroy_all
 Booking.destroy_all
+User.destroy_all
 
-sarah  = User.create! (
-  first_name: 'Sarah',
+sarah = User.create(
+  {first_name: 'Sarah',
   last_name: 'Smith',
   email: 'sarah_smith@gmail.com',
-  password: 'hello123')
+  password: 'hello123'})
 
-nathalie = User.create! (
-  first_name: 'Nathalie',
+nathalie = User.create(
+  {first_name: 'Nathalie',
   last_name: 'Faber',
   email: 'nathaliefaber@hotmail.com',
-  password: 'funkystuff123')
+  password: 'funkystuff123'})
 
 puts '2 users created'
 
-booking_1 = booking.create!(
-  start_datetime: '2 May 2022',
-  end_datetime: '3 May 2022',
+art_service1 = ArtService.create(
+  {title: 'ballet',
+  description: '2 hour beautiful ballet show in your living room',
+  price: '350€',
+  address: 'Berlin Mitte',
+  category: 'dancing',
+  user: sarah}
+)
+
+art_service2 = ArtService.create(
+  {title: 'portrait painting',
+  description: 'have the portraits of your loved ones painted!',
+  price: '280€',
+  address: 'Berlin Kreuzberg',
+  category: 'painting',
+  user: sarah}
+)
+
+art_service3 = ArtService.create(
+  {title: 'opera singing',
+  description: 'wonderful 2 hour opera concert',
+  price: '400€',
+  address: 'Berlin Charlottenburg',
+  category: 'singing',
+  user: nathalie}
+)
+
+puts '3 services created'
+
+booking_1 = Booking.create(
+  {start_time: DateTime.parse('2 May 2022'),
+  end_time: DateTime.parse('3 May 2022'),
   status: 'confirmed',
-  user: nathalie,
+  user_id: nathalie.id,
+  art_service_id: art_service1.id}
 )
 
-booking_2 = booking.create!(
-  start_datetime: '10 Nov 2022',
-  end_datetime: '11 Nov 2022',
+booking_2 = Booking.create(
+  {start_time: DateTime.parse('10 Nov 2022'),
+  end_time: DateTime.parse('11 Nov 2022'),
   status: 'pending',
-  user: sarah,
+  user_id: sarah.id,
+  art_service_id: art_service2.id}
 )
 
-booking_3 = booking.create!(
-  start_datetime: '22 Dec 2022',
-  end_datetime: '22 Dec 2022',
+booking_3 = Booking.create(
+  {start_time: DateTime.parse('21 Dec 2020'),
+  end_time: DateTime.parse('22 Dec 2022'),
   status: 'cancelled',
-  user: sarah,
+  user_id: sarah.id,
+  art_service_id: art_service3.id}
 )
 puts '3 bookings created'
-
-art_service_1 = art_service.create!(
-  title:
-  description:
-  price:
-  address:
-  category:
-)
-
-art_service_1 = art_service.create!(
-  title:
-  description:
-  price:
-  address:
-  category:
-)
-
-art_service_1 = art_service.create!(
-  title:
-  description:
-  price:
-  address:
-  category:
-)
