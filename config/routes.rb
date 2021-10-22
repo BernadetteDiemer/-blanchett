639dd7bca88826249127ee5a519275fc49403532
Bernadette Diemer <diemer.lulu@gmail.com>
2021-10-22 16:17:01 +0200

    created the booking routes for #new and #create, in order to have a functioning button on the art_service#show-page, integrated Linda's images, almost destroyed my computer, struggled an unreasonable amount with front-end

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :art_services, only: [:new, :create, :show]
-
+  resources :bookings, only: [:new, :create]
 end
