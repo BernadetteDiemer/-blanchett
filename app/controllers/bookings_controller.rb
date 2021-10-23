a7f4cc7a6a70c588038148281a218b9abb16b91e
Virna Harri <harrivirna18@gmail.com>
2021-10-23 11:22:22 +0200

    in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,33 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user! # virna
+
+  def new
+    @booking = Booking.new(booking_params)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    authorize @booking
+    @booking.user = current_user
+    @booking.art_service = ArtService.find(params[:art_service_id])
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      redirect_to art_service_path(@artservice)
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_artservice
+    @art_service = ArtService.find(params[artservice_id])
+  end
+
+  def booking_params
+    params[:booking].permit(:start_time, :end_time, :status)
+  end
 end
