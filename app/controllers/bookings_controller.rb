431bba8bb2f5c9cdc8db1d5c7ec9543056ac5d8f
Virna Harri <harrivirna18@gmail.com>
2021-10-25 11:43:42 +0200

    Simple form for new booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,31 +1,38 @@
 class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :home, :index]
+  # before_action :set_booking, only: [:create]
+  # before_action :booking_params, only: [:create]
+
+  def index
+    # @bookings = Booking.all
+    @bookings = policy_scope(Booking)
+  end
+
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
     authorize @booking
     @booking.user = current_user
-    @booking.art_service = ArtService.find(params[:art_service_id])
+    art_service = ArtService.find(params[:art_service_id])
+    @booking.art_service = art_service
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to bookings_path(@bookings)
     else
-      redirect_to art_service_path(@artservice)
+      render :new
     end
   end
 
   private
 
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
-
-  def set_artservice
-    @art_service = ArtService.find(params[:art_service_id])
-  end
+  # def set_booking
+  #   @booking = Booking.find(params[:id])
+  # end
 
   def booking_params
-    params[:booking].permit(:start_time, :end_time, :status)
+    params[:booking].require(booking_params)
   end
 end
