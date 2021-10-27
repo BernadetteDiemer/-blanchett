d78aff14594bf62ea3d5efbceaf2337db8841803
linda dao <lindadao92@gmail.com>
2021-10-27 12:51:42 +0200

    list my art-services in the profile page, upload profile picture, style fixes in form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/user_information/:id', to: 'pages#user_profile', as: "profile"
+  patch '/user_information/:id', to: 'pages#update', as: "profile_update"
   resources :bookings, only: :index
 
   resources :art_services do
